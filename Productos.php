f9772fc5ee0db55d44d50366b58d3305c415ad9b
Ricardo Tapia <RickTC@localhost.localdomain>
2014-03-22 01:23:27 -0600

    Practica 6 productos.

---
+++ Productos.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+<form method="POST" action="Guardar.php">
+
+<table border=1>
+	<tr>
+		<td> </td>
+		<td>Producto</td>
+		<td>$ Precio</td>
+	</tr>
+	
+	<tr>
+		<td>1</td>
+		<td><input type = "text" name="Producto1" id="Producto1" size="20"></td>
+		<td><input type = "text" name="Precio1" id="Precio1" size="20"></td>
+	</tr>
+	
+	<tr>
+		<td>2</td>
+		<td><input type = "text" name="Producto2" id="Producto2" size="20"></td>
+		<td><input type = "text" name="Precio2" id="Precio2" size="20"></td>	
+	</tr>
+	
+	<tr>
+		<td>3</td>
+		<td><input type = "text" name="Producto3" id="Producto3" size="20"></td>
+		<td><input type = "text" name="Precio3" id="Precio3" size="20"></td>	
+	</tr>
+	
+</table>
+<input type="submit" value="Enviar">
+</from>
+</body>
+</html>
+ 
